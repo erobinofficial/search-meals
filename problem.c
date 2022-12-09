753a291173aa2a3ea275a7b2e4ed719ff2402c18
erobinofficial <erobin8778@gmail.com>
2022-12-09 21:24:41 +0600

    sdfas

--- problem.c
+++ problem.c
@@ -2,12 +2,7 @@
 int main(){
     int x, vag;
     scanf("%d",&x);
-    vag = x/5;
-    switch (vag){
-        case 19:
-            printf("A+");
-            break;
-            printf("F");
+    vag = x
             break;
     }
 }
